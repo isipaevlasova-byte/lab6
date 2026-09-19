46279b68abc9d8dbec02d99bd344f03397078985
sasha <isip_a.e.vlasova@mpt.ru>
2026-09-19 14:07:36 +0300

    Update self.c

--- self.c
+++ self.c
@@ -2,25 +2,36 @@
 // Студентка: Власова Александра Евгеньевна
 // Назначение: Цикл for и вложенный цикл
 #include <stdio.h>
-int main(void) {
-    int h, w;
-    if (scanf("%d %d", &h, &w) != 2) {
-        printf("Input error\n");
-        return 0;
-    }
-    if (h < 2 || h > 10 || w < 2 || w > 10) {
-        printf("Range error\n");
-        return 0;
-    }
-    for (int row = 1; row <= h; row++) {
-        for (int col = 1; col <= w; 
-            if (row == 1 || row == h || col == 1 || col == w) {
-                printf("*");
-            } else {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
+int main(void) { int h, w;
+if (scanf("%d %d", &h, &w) != 2)
+{
+    printf("Input error");
     return 0;
+}
+
+if (h < 2 || h > 10 || w < 2 || w > 10)
+{
+    printf("Range error");
+    return 0;
+}
+
+for (int i = 1; i <= h; i++)
+{
+    for (int j = 1; j <= w; j++)
+    {
+        if (i == 1 2 || w > 10)
+j == 1 || j == w)
+        {
+            printf("*");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+
+    printf("\n");
+}
+
+return 0;
 }
